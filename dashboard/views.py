aa4a2f89748d6c7d031ef51acde2a6397d713e0a
Dariusz Ryszka <dariusz.ryszka@onet.pl>
2022-10-12 16:35:01 +0200

    Add YouTube function in views and class Dashboard in forms

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from .forms import *
 from django.contrib import messages
 from django.views import generic
+from youtubesearchpython import VideosSearch
 
 
 # Create your views here.
@@ -81,4 +82,34 @@ def delete_homework(request, pk=None):
 
 
 def youtube(request):
-    return render(request, "dashboard/youtube.html")
+    if request.method == 'POST':
+        form = DashboardForm(request.POST)
+        text = request.Post['text']
+        video = VideosSearch(text, limit=10)
+        result_list = []
+        for i in video.result()['result']:
+            result_dict = {
+                'input': text,
+                'title': i['title'],
+                'duration': i['duration'],
+                'thumbnail': i['thumbnail'][0]['url'],
+                'channel': i['channel']['name'],
+                'link': i['link'],
+                'views': i['viewcount']['short'],
+                'published': i['publishedTime'],
+            }
+            desc = ''
+            if i['descriptionSnippet']:
+                for j in i['descriptionSnippet']:
+                    desc += j['text']
+            result_dict['description'] = desc
+            result_list.append(result_dict)
+            context = {
+                'form': form,
+                'results': result_list
+            }
+        return render(request, 'dashboard/youtube.html', context)
+    else:
+        form = DashboardForm()
+    context = {'form': form}
+    return render(request, "dashboard/youtube.html", context)
